8df690307e15b53cc63c496c479f3450a3a732c4
Vlad Truta <vlad.truta@tapptitude.com>
2020-05-19 22:12:00 +0300

    Incremented database version

--- app/src/main/java/com/vladtruta/restaurantmenu/data/persistence/RestaurantDatabase.kt
+++ app/src/main/java/com/vladtruta/restaurantmenu/data/persistence/RestaurantDatabase.kt
@@ -8,7 +8,7 @@ import com.vladtruta.restaurantmenu.data.model.local.*
 
 @Database(
     entities = [Category::class, MenuCourse::class, CartItem::class, Customer::class, OrderedItem::class],
-    version = 7,
+    version = 8,
     exportSchema = false
 )
 abstract class RestaurantDatabase : RoomDatabase() {
