52bce85b627be583a71d9ce79c2ad4e856573626
Vlad Truta <vlad.truta@tapptitude.com>
2020-05-19 23:54:04 +0300

    Small refactor

--- app/src/main/java/com/vladtruta/restaurantmenu/data/model/responses/CustomerResponse.kt
+++ app/src/main/java/com/vladtruta/restaurantmenu/data/model/responses/CustomerResponse.kt
@@ -1,10 +1,14 @@
 package com.vladtruta.restaurantmenu.data.model.responses
 
+import com.google.gson.annotations.SerializedName
 import com.vladtruta.restaurantmenu.data.model.local.Customer
 
 data class CustomerResponse(
+    @SerializedName("fullName")
     val fullName: String? = null,
+    @SerializedName("tableName")
     val tableName: String? = null,
+    @SerializedName("id")
     val id: Int? = null
 ) {
     fun toCustomer(): Customer? {
